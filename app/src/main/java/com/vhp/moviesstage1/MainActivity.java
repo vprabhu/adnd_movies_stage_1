8600638dc89b6120131837e1867ae4493ce61c72
vignesh prabhu <vignesh.p@ninestars.in>
2017-07-14 12:15:05 +0530

    feat : initial commit

---
+++ app/src/main/java/com/vhp/moviesstage1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vhp.moviesstage1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mMoviesRecyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMoviesRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_movies);
+    }
+}
