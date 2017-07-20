191b00b1cff5779cbd72b0fa7bccb1a2beaeb176
prabhu <sekarvprabhu@gmail.com>
2017-07-20 07:06:44 +0530

    feat : fetch json object

--- app/src/main/java/com/vhp/moviesstage1/MainActivity.java
+++ app/src/main/java/com/vhp/moviesstage1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.vhp.moviesstage1;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.vhp.moviesstage1.utils.Constants;
 import com.vhp.moviesstage1.utils.NetworkUtils;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private RecyclerView mMoviesRecyclerView;
     @Override
@@ -17,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
 
         mMoviesRecyclerView = (RecyclerView) findViewById(R.id.recyclerView_movies);
 
-        NetworkUtils.buildUrl(Constants.MOVIES_POPULAR);
+        new MoviesListAsyncTask().execute();
 
+    }
+
+    private class MoviesListAsyncTask extends AsyncTask<URL , String  , String>{
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL moviesUrl = NetworkUtils.buildUrl(Constants.MOVIES_POPULAR);
+
+            String moviesResult = null;
+            try {
+                moviesResult = NetworkUtils.getResponseFromHttpUrl(moviesUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return moviesResult;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: " + s);
+
+        }
     }
 }
--- app/src/main/java/com/vhp/moviesstage1/utils/NetworkUtils.java
+++ app/src/main/java/com/vhp/moviesstage1/utils/NetworkUtils.java
@@ -3,10 +3,14 @@ package com.vhp.moviesstage1.utils;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.Scanner;
 
 /**
  * Created by root on 14/7/17.
@@ -50,6 +54,30 @@ public class NetworkUtils {
         return moviesUrl;
     }
 
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
 
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 
 }
